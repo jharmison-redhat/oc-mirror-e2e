1dd6025d343490e533e9d4e37fde3dae311b8748
James Harmison <jharmison@redhat.com>
2022-01-14 16:08:29 -0500

    Changed to env/ for runner

--- example/run.sh
+++ example/run.sh
@@ -5,17 +5,21 @@ cd "$(dirname "$(realpath "$0")")" || exit
 podman_args=(--rm -it)
 
 # We need to run as the user to just not deal with labelling
-podman_args+=(--privileged --security-opt=label=disable)
+podman_args+=(--privileged --security-opt=label=disable --name=oc-mirror-e2e)
 
 # AWS configuration depends on exporting user variables as well as the credentials directory
+cat << EOF > env/envvars
+---
+AWS_CONFIG_FILE: /aws/config
+AWS_SHARED_CREDENTIALS_FILE: /aws/credentials
+EOF
+for var in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_PROFILE; do
+    if [ -n "${!var}" ]; then
+        echo "$var: ${!var}" >> env/envvars
+    fi
+done
 podman_args+=(
-    -e AWS_CONFIG_FILE=/aws/config
-    -e AWS_SHARED_CREDENTIALS_FILE=/aws/credentials
-    -e AWS_ACCESS_KEY_ID
-    -e AWS_SECRET_ACCESS_KEY
-    -e AWS_SESSION_TOKEN
-    -e AWS_PROFILE
-    -v "$HOME/.aws:/aws"
+   -v "$HOME/.aws:/aws"
 )
 
 # We need to save output, this is the default for the collection
@@ -28,7 +32,7 @@ podman_args+=(
     -e RUNNER_PLAYBOOK=jharmison_redhat.oc_mirror_e2e.create
 )
 
-# Runner will pull our inventory and config
+# Runner will pull our inventory, config, and env
 podman_args+=(
     -v "$PWD:/runner"
 )
