5c303e939f3ee0988b5e9b06bef68944c75086cb
James Harmison <jharmison@redhat.com>
2022-01-17 17:32:17 -0500

    Pass disk size through root module

--- roles/provisioner/files/terraform/main.tf
+++ roles/provisioner/files/terraform/main.tf
@@ -1,9 +1,10 @@
 module "testbed" {
   source = "git://github.com/jharmison-redhat/disconnected-openshift-testbed.git?ref=0.1.1"
 
-  public_key     = var.public_key
-  cluster_name   = var.cluster_name
-  cluster_domain = var.cluster_domain
+  public_key       = var.public_key
+  cluster_name     = var.cluster_name
+  cluster_domain   = var.cluster_domain
+  registry_disk_gb = var.registry_disk_gb
 }
 
 provider "aws" {
