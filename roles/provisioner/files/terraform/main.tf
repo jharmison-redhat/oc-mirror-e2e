774be965b0f43ae42e73b9223802168ab0ed3409
James Harmison <jharmison@redhat.com>
2022-01-19 17:09:46 -0500

    Inherit private DNS from terraform

--- roles/provisioner/files/terraform/main.tf
+++ roles/provisioner/files/terraform/main.tf
@@ -1,5 +1,5 @@
 module "testbed" {
-  source = "git://github.com/jharmison-redhat/disconnected-openshift-testbed.git?ref=0.2.0"
+  source = "git://github.com/jharmison-redhat/disconnected-openshift-testbed.git?ref=0.2.1"
 
   public_key       = var.public_key
   cluster_name     = var.cluster_name
