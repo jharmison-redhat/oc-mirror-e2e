7447ca4d2980715f3a7fb380159324938c83b316
James Harmison <jharmison@redhat.com>
2022-02-08 14:40:16 -0500

    Enable teardown to use venv

--- example/teardown.sh
+++ example/teardown.sh
@@ -1,55 +1,57 @@
 #!/bin/bash
 
-read -sp "WARNING! This script does not save your Terraform state. Are you sure you want to destroy the terraformed infrastructure, without recovering state? Press Enter to continue, or Ctrl+C to cancel." throwaway
+read -rsp "WARNING! This script does not save your Terraform state. Are you sure you want to destroy the terraformed infrastructure, without recovering state? Press Enter to continue, or Ctrl+C to cancel." throwaway
 echo
+
+cd "$(dirname "$(realpath "$0")")" || exit
 
 set -x
 
 cluster=disco
 domain=redhat4govaws.io
 
-domain_hyphenated=$(echo "$domain" | tr '.' '-')
 domain_underscored=$(echo "$domain" | tr '.' '_')
 
+awscli="$PWD/../.venv/bin/aws"
 
-aws s3 rb --force "s3://${cluster}-${domain}-registry"
+"$awscli" s3 rb --force "s3://${cluster}-${domain}-registry"
 
-for instance in $(aws ec2 describe-instances --filters Name=tag:Project,Values=disconnected-openshift-testbed Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId" --output text); do
-    for eip in $(aws ec2 describe-addresses --filter Name=instance-id,Values="$instance" --query "Addresses[*].AllocationId" --output text); do
-        assoc=$(aws ec2 describe-addresses --filter Name=allocation-id,Values="$eip" --query "Addresses[*].AssociationId" --output text)
-        aws ec2 disassociate-address --association-id "$assoc"
-        aws ec2 release-address --allocation-id "$eip"
+for instance in $("$awscli" ec2 describe-instances --filters Name=tag:Project,Values=disconnected-openshift-testbed Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].InstanceId" --output text); do
+    for eip in $("$awscli" ec2 describe-addresses --filter Name=instance-id,Values="$instance" --query "Addresses[*].AllocationId" --output text); do
+        assoc=$("$awscli" ec2 describe-addresses --filter Name=allocation-id,Values="$eip" --query "Addresses[*].AssociationId" --output text)
+        "$awscli" ec2 disassociate-address --association-id "$assoc"
+        "$awscli" ec2 release-address --allocation-id "$eip"
     done
-    aws ec2 terminate-instances --instance-ids "$instance"
+    "$awscli" ec2 terminate-instances --instance-ids "$instance"
 done
 
-aws ec2 delete-key-pair --key-name "${cluster}_${domain_underscored}"
+"$awscli" ec2 delete-key-pair --key-name "${cluster}_${domain_underscored}"
 
-for vpc in $(aws ec2 describe-vpcs --filter Name=tag:Project,Values=disconnected-openshift-testbed --query "Vpcs[*].VpcId" --output text); do
-    for sg in $(aws ec2 describe-security-groups --filter "Name=vpc-id,Values=$vpc" "Name=group-name,Values=registry.${cluster}.${domain}" --query "SecurityGroups[*].GroupId" --output text); do
-        aws ec2 delete-security-group --group-id "$sg"
+for vpc in $("$awscli" ec2 describe-vpcs --filter Name=tag:Project,Values=disconnected-openshift-testbed --query "Vpcs[*].VpcId" --output text); do
+    for sg in $("$awscli" ec2 describe-security-groups --filter "Name=vpc-id,Values=$vpc" "Name=group-name,Values=registry.${cluster}.${domain}" --query "SecurityGroups[*].GroupId" --output text); do
+        "$awscli" ec2 delete-security-group --group-id "$sg"
     done
-    for igw in $(aws ec2 describe-internet-gateways --filter "Name=attachment.vpc-id,Values=$vpc" --query "InternetGateways[*].InternetGatewayId" --output text); do
-        aws ec2 detach-internet-gateway --internet-gateway-id "$igw" --vpc-id "$vpc"
-        aws ec2 delete-internet-gateway --internet-gateway-id "$igw"
+    for igw in $("$awscli" ec2 describe-internet-gateways --filter "Name=attachment.vpc-id,Values=$vpc" --query "InternetGateways[*].InternetGatewayId" --output text); do
+        "$awscli" ec2 detach-internet-gateway --internet-gateway-id "$igw" --vpc-id "$vpc"
+        "$awscli" ec2 delete-internet-gateway --internet-gateway-id "$igw"
     done
-    for rt in $(aws ec2 describe-route-tables --filter "Name=vpc-id,Values=$vpc" Name=association.main,Values=false --query "RouteTables[*].RouteTableId" --output text); do
-        for assoc in $(aws ec2 describe-route-tables --route-table-ids "$rt" --query "RouteTables[*].Associations[*].RouteTableAssociationId" --output text); do
-            aws ec2 disassociate-route-table --association-id "$assoc"
+    for rt in $("$awscli" ec2 describe-route-tables --filter "Name=vpc-id,Values=$vpc" Name=association.main,Values=false --query "RouteTables[*].RouteTableId" --output text); do
+        for assoc in $("$awscli" ec2 describe-route-tables --route-table-ids "$rt" --query "RouteTables[*].Associations[*].RouteTableAssociationId" --output text); do
+            "$awscli" ec2 disassociate-route-table --association-id "$assoc"
         done
-        aws ec2 delete-route-table --route-table-id "$rt"
+        "$awscli" ec2 delete-route-table --route-table-id "$rt"
     done
-    for sn in $(aws ec2 describe-subnets --filter "Name=vpc-id,Values=$vpc" --query "Subnets[*].SubnetId" --output text); do
-        aws ec2 delete-subnet --subnet-id "$sn"
+    for sn in $("$awscli" ec2 describe-subnets --filter "Name=vpc-id,Values=$vpc" --query "Subnets[*].SubnetId" --output text); do
+        "$awscli" ec2 delete-subnet --subnet-id "$sn"
     done
-    aws ec2 delete-vpc --vpc-id "$vpc"
+    "$awscli" ec2 delete-vpc --vpc-id "$vpc"
 done
 
-for hz in $(aws route53 list-hosted-zones | jq -r '.HostedZones[]|select(.Name == "'"$domain"'.").Id'); do
+for hz in $("$awscli" route53 list-hosted-zones | jq -r '.HostedZones[]|select(.Name == "'"$domain"'.").Id'); do
     change_batch=$(mktemp)
     for a_record in registry. proxy.; do
-        for rrs_name in $(aws route53 list-resource-record-sets --hosted-zone-id "$hz" | jq -r '.ResourceRecordSets[]|select(.Name|startswith("'"$a_record"'")).Name' || echo ""); do
-            rrs=$(aws route53 list-resource-record-sets --hosted-zone-id "$hz" | jq '.ResourceRecordSets[]|select(.Name == "'"$rrs_name"'")') ||:
+        for rrs_name in $("$awscli" route53 list-resource-record-sets --hosted-zone-id "$hz" | jq -r '.ResourceRecordSets[]|select(.Name|startswith("'"$a_record"'")).Name' || echo ""); do
+            rrs=$("$awscli" route53 list-resource-record-sets --hosted-zone-id "$hz" | jq '.ResourceRecordSets[]|select(.Name == "'"$rrs_name"'")') ||:
             if [ -n "$rrs" ]; then
                 cat << EOF > "$change_batch"
 {
@@ -60,18 +62,18 @@ for hz in $(aws route53 list-hosted-zones | jq -r '.HostedZones[]|select(.Name =
     }]
 }
 EOF
-                aws route53 change-resource-record-sets --hosted-zone-id "$hz" --change-batch "file://$change_batch"
+                "$awscli" route53 change-resource-record-sets --hosted-zone-id "$hz" --change-batch "file://$change_batch"
             fi
         done
     done
     rm -f "$change_batch"
 done
 
-for hz in $(aws route53 list-hosted-zones | jq -r '.HostedZones[]|select(.Name == "'"internal.$cluster.$domain"'.").Id'); do
+for hz in $("$awscli" route53 list-hosted-zones | jq -r '.HostedZones[]|select(.Name == "'"internal.$cluster.$domain"'.").Id'); do
     change_batch=$(mktemp)
     for a_record in registry. proxy. bastion.; do
-        for rrs_name in $(aws route53 list-resource-record-sets --hosted-zone-id "$hz" | jq -r '.ResourceRecordSets[]|select(.Name|startswith("'"$a_record"'")).Name' || echo ""); do
-            rrs=$(aws route53 list-resource-record-sets --hosted-zone-id "$hz" | jq '.ResourceRecordSets[]|select(.Name == "'"$rrs_name"'")') ||:
+        for rrs_name in $("$awscli" route53 list-resource-record-sets --hosted-zone-id "$hz" | jq -r '.ResourceRecordSets[]|select(.Name|startswith("'"$a_record"'")).Name' || echo ""); do
+            rrs=$("$awscli" route53 list-resource-record-sets --hosted-zone-id "$hz" | jq '.ResourceRecordSets[]|select(.Name == "'"$rrs_name"'")') ||:
             if [ -n "$rrs" ]; then
                 cat << EOF > "$change_batch"
 {
@@ -82,28 +84,28 @@ for hz in $(aws route53 list-hosted-zones | jq -r '.HostedZones[]|select(.Name =
     }]
 }
 EOF
-                aws route53 change-resource-record-sets --hosted-zone-id "$hz" --change-batch "file://$change_batch"
+                "$awscli" route53 change-resource-record-sets --hosted-zone-id "$hz" --change-batch "file://$change_batch"
             fi
         done
     done
     rm -f "$change_batch"
-    aws route53 delete-hosted-zone --id "$hz"
+    "$awscli" route53 delete-hosted-zone --id "$hz"
 done
 
-for ak in $(aws iam list-access-keys --user-name "${cluster}.${domain}-registry" --query 'AccessKeyMetadata[*].AccessKeyId' --output text); do
-    aws iam delete-access-key --user-name "${cluster}.${domain}-registry" --access-key-id "$ak"
+for ak in $("$awscli" iam list-access-keys --user-name "${cluster}.${domain}-registry" --query 'AccessKeyMetadata[*].AccessKeyId' --output text); do
+    "$awscli" iam delete-access-key --user-name "${cluster}.${domain}-registry" --access-key-id "$ak"
 done
-for policy in $(aws iam list-attached-user-policies --user-name "${cluster}.${domain}-registry" --query "AttachedPolicies[*].PolicyArn" --output text); do
-    aws iam detach-user-policy --user-name "${cluster}.${domain}-registry"  --policy-arn "$policy"
-    aws iam delete-policy --policy-arn "$policy"
+for policy in $("$awscli" iam list-attached-user-policies --user-name "${cluster}.${domain}-registry" --query "AttachedPolicies[*].PolicyArn" --output text); do
+    "$awscli" iam detach-user-policy --user-name "${cluster}.${domain}-registry"  --policy-arn "$policy"
+    "$awscli" iam delete-policy --policy-arn "$policy"
 done
-aws iam delete-user --user-name "${cluster}.${domain}-registry"
+"$awscli" iam delete-user --user-name "${cluster}.${domain}-registry"
 
-for ak in $(aws iam list-access-keys --user-name "${cluster}.${domain}-ocp-installer" --query 'AccessKeyMetadata[*].AccessKeyId' --output text); do
-    aws iam delete-access-key --user-name "${cluster}.${domain}-ocp-installer" --access-key-id "$ak"
+for ak in $("$awscli" iam list-access-keys --user-name "${cluster}.${domain}-ocp-installer" --query 'AccessKeyMetadata[*].AccessKeyId' --output text); do
+    "$awscli" iam delete-access-key --user-name "${cluster}.${domain}-ocp-installer" --access-key-id "$ak"
 done
-for policy in $(aws iam list-attached-user-policies --user-name "${cluster}.${domain}-ocp-installer" --query "AttachedPolicies[*].PolicyArn" --output text); do
-    aws iam detach-user-policy --user-name "${cluster}.${domain}-ocp-installer"  --policy-arn "$policy"
-    aws iam delete-policy --policy-arn "$policy"
+for policy in $("$awscli" iam list-attached-user-policies --user-name "${cluster}.${domain}-ocp-installer" --query "AttachedPolicies[*].PolicyArn" --output text); do
+    "$awscli" iam detach-user-policy --user-name "${cluster}.${domain}-ocp-installer"  --policy-arn "$policy"
+    "$awscli" iam delete-policy --policy-arn "$policy"
 done
-aws iam delete-user --user-name "${cluster}.${domain}-ocp-installer"
+"$awscli" iam delete-user --user-name "${cluster}.${domain}-ocp-installer"
